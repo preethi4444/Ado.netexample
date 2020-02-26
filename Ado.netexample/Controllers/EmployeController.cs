ba968093d26bd7ca99c7b61fccda44ea7b4d5486
SaiPreethi Induri <Saipreethi_Induri@epam.com>
2020-02-26 13:12:33 +0530

    Add project files.

---
+++ Ado.netexample/Controllers/EmployeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ado.netexample.Models;
+namespace Ado.netexample.Controllers
+{
+    public class EmployeController : Controller
+    {
+        // GET: Employe
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Employe/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Employe/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employe/Create
+        [HttpPost]
+        public ActionResult Create(Employe emp)
+        {
+            //try
+            //{
+                // TODO: Add insert logic here
+                Employe e = new Employe();
+
+                e.Add_records(emp);
+
+                return RedirectToAction("Index");
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
+        }
+
+        // GET: Employe/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employe/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employe/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employe/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Ado.netexample/Models/Employe.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Ado.netexample.Models
+{
+    public class Employe
+    {
+    
+        public string Name { get; set; }
+        public int Mobileno{ get; set; }
+        SqlConnection obj;
+
+        public Employe()
+        {
+             obj = new SqlConnection(ConfigurationManager.ConnectionStrings["Employecon"].ConnectionString);
+
+        }
+        public void Add_records(Employe e)
+        {
+            SqlCommand com = new SqlCommand("AddNewEmpDetails", obj);
+            com.CommandType = CommandType.StoredProcedure;
+            
+            com.Parameters.AddWithValue("@Name", e.Name);
+            com.Parameters.AddWithValue("@Mobileno", e.Mobileno);
+            obj.Open();
+            com.ExecuteNonQuery();
+          
+        }
+    }
+}
